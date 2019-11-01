a1ad3bbb595397353f4a3cfda682b9da3eebf36a
Leonardo Vargas <leu1607@hotmail.com>
2019-10-31 23:49:09 -0300

    Terminando de configurar a api do json-server e iniciando as configurações do reducer

--- arquitetura-flux-carrinho-de-compras/src/pages/Home/index.js
+++ arquitetura-flux-carrinho-de-compras/src/pages/Home/index.js
@@ -1,85 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-direct-masculino/20/D22-2835-120/D22-2835-120_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tênis Topper</strong>
-        <span>R$ 1.000,00</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 10
-          </div>
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-direct-masculino/20/D22-2835-120/D22-2835-120_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tênis Tope Tope Tope Tope Tope Tope Tope Tope Tope Tope</strong>
-        <span>R$ 1.000,00</span>
+  async componentDidMount() {
+    const response = await api.get('products');
+    const products = response.data.map(p => ({
+      ...p,
+      priceFormatted: formatPrice(p.price),
+    }));
+    this.setState({ products });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 10
-          </div>
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-direct-masculino/20/D22-2835-120/D22-2835-120_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tênis Topper</strong>
-        <span>R$ 1.000,00</span>
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 10
-          </div>
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-direct-masculino/20/D22-2835-120/D22-2835-120_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tênis Topper</strong>
-        <span>R$ 1.000,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 10
-          </div>
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-direct-masculino/20/D22-2835-120/D22-2835-120_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tênis Topper</strong>
-        <span>R$ 1.000,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 10
-          </div>
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 10
+              </div>
+              <span>Adicionar ao Carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
